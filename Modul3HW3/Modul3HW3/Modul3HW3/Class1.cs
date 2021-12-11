5711c0ea17a776fb06d72cd073dff38c29518cb8
Sofia_Rutska <sofiia.rutska@nure.ua>
2021-12-11 22:04:22 +0200

    some

---
+++ Modul3HW3/Modul3HW3/Modul3HW3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW3
+{
+    public class Class1
+    {
+        public delegate void SomeDel(bool x);
+
+        public static int Pow(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Modul3HW3/Modul3HW3/Modul3HW3/Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW3
+{
+    public class Class2
+    {
+        private NewDelegate _newDelegate;
+        private static int _item;
+
+        public delegate int MyOwnDelegate(int x, int y);
+        public delegate bool NewDelegate(int x);
+
+        public static bool Result(int x)
+        {
+            if (_item % x == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public NewDelegate Calc(int x, int y, MyOwnDelegate handle)
+        {
+            _item = handle.Invoke(x, y);
+            _newDelegate = Result;
+            return _newDelegate;
+        }
+    }
+}
