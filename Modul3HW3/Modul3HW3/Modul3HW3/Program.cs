3a9be21ca6b8816617a84d83429085a0dc21452f
Sofia_Rutska <sofiia.rutska@nure.ua>
2021-12-12 07:58:43 +0200

    Done

--- Modul3HW3/Modul3HW3/Modul3HW3/Program.cs
+++ Modul3HW3/Modul3HW3/Modul3HW3/Program.cs
@@ -7,10 +7,10 @@ namespace Modul3HW3
         public static void Main(string[] args)
         {
             var result = new Class2();
-            var myDel = new Class1.SomeDel(Show);
+            var show = new Class1.SomeDel(Show);
             var myClass2Del = new Class2.MyOwnDelegate(Class1.Pow);
-            var myClass2NewDel = result.Calc(2, 3, myClass2Del);
-            myDel.Invoke(myClass2NewDel.Invoke(2));
+            var myDel = result.Calc(2, 3, myClass2Del);
+            show.Invoke(myDel.Invoke(2));
         }
 
         public static void Show(bool x)
